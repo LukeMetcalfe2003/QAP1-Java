18955e219d9de1aad561586a68200bcffdde3db6
Luke Metcalfe <luke.metcalfe@keyin.com>
2024-09-17 16:26:48 -0230

    Added the Java files for problem two.

---
+++ TestDate.java
@@ -0,0 +1,7 @@
+public class TestDate {
+    public static void main(String[] args) {
+        Date date = new Date(17, 9, 2024);
+
+        System.out.println("The date is: " + date.dateString());
+    }
+}
